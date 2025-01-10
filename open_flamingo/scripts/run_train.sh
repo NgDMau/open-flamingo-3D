327e7c46dbe20930b184e87253e8fb0feae756b3
NgDMau <maunguyen.dinh@gmail.com>
2025-01-10 06:57:26 +0000

    Update username

--- open_flamingo/scripts/run_train.sh
+++ open_flamingo/scripts/run_train.sh
@@ -30,3 +30,25 @@ srun --cpu_bind=v --accel-bind=gn python open_flamingo/open_flamingo/train/train
     --mmc4_shards "/path/to/shards/shard-{0000..0999}.tar" \
     --gradient_checkpointing \
     --report_to_wandb \
+
+
+
+# srun --cpu_bind=v --accel-bind=gn python open_flamingo/open_flamingo/train/train.py \
+#     --lm_path anas-awadalla/mpt-1b-redpajama-200b \
+#     --tokenizer_path anas-awadalla/mpt-1b-redpajama-200b \
+#     --cross_attn_every_n_layers 1 \
+#     --dataset_resampled \
+#     --batch_size_mmc4 32 \
+#     --batch_size_laion 64 \
+#     --train_num_samples_mmc4 125000\
+#     --train_num_samples_laion 250000 \
+#     --loss_multiplier_laion 0.2 \
+#     --workers=4 \
+#     --run_name OpenFlamingo-3B-vitl-mpt1b \
+#     --num_epochs 480 \
+#     --warmup_steps  1875 \
+#     --mmc4_textsim_threshold 0.24 \
+#     --laion_shards "/path/to/shards/shard-{0000..0999}.tar" \
+#     --mmc4_shards "/path/to/shards/shard-{0000..0999}.tar" \
+#     --gradient_checkpointing \
+#     --report_to_wandb \
